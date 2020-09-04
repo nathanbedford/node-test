74aec8b835f3ab37017ebae0f9f6b52b91b32e8a
Nathan Bedford <nathan.bedford@gmail.com>
2020-09-04 10:06:21 -0400

    Testing

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World! This is pretty cool'));
+app.get("/", (req, res) =>
+  res.send(
+    "Hello World! This is pretty cool. Learning something new every day."
+  )
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
